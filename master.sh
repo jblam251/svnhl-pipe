630293abfd89269696599e8483f146cf10e14db7
Jacob Blamer <43484807+jblam251@users.noreply.github.com>
2019-07-16 14:58:30 -0400

    Add files via upload

--- master.sh
+++ master.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
-#SBATCH --time=00:45:00
-#SBATCH --mem-per-cpu=8000M
-#SBATCH --ntasks=2
 
+module load sickle
+module load samtools
+module load samblaster
+module load bwa
+module load bcftools
 module load gcc/5.4.0
 module load cnvnator/0.3.3
 module load root/6.14.04
-module load delly
 module load breakdancer/1.4.5
-module load bwa
-module load samtools
-module load bcftools
-module load sickle
-module load samblaster
+module load vcftools/0.1.14
+module load delly
 
-REF="/home/jblamer/projects/def-lukens/jblamer/ref/canFam3.fa"
-DATA="/home/jblamer/projects/def-lukens/jblamer/data/hundKfq_sample"
-BIN="/home/jblamer/projects/def-lukens/jblamer/bin"
+REF="jblamer/ref/canFam3.fa"
+BIN="jblamer/bin"
+DATA="jblamer/scratch/scratch_cases"
 
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 cd $DATA
-gunzip *.gz
-
 
 ###### SICKLE ############################ #######
 ###################################################
-
-parallel sickle pe -f {}_R1.fastq -r {}_R2.fastq -t sanger -o {}_R1.fltr.fastq -p {}_R2.fltr.fastq ::: $(ls -1 *_R1.fastq | sed 's/_R1.fastq//')
+sickle pe -f $1_R1.fastq -r $1_R2.fastq -t sanger -o $1_R1.fltr.fastq -p $1_R2.fltr.fastq -s $1_singles.fltr.fastq
 	
 	if [ $? -ne 0 ]
 	then				
@@ -42,23 +38,8 @@ parallel sickle pe -f {}_R1.fastq -r {}_R2.fastq -t sanger -o {}_R1.fltr.fastq -
 ###### BWA ALLIGNMENT & SAMBLASTER ################
 ###################################################
 
-parallel bwa mem -R "@RG'\t'ID:{}_ID'\t'LB:{}_LB'\t'PL:illumna" $REF {}_R1.fltr.fastq {}_R2.fltr.fastq "|" samblaster ">" {}.sam ::: $(ls -1 *_R1.fltr.fastq | sed 's/_R1.fltr.fastq//')	
+bwa mem -t 16 -R $(echo "@RG\tID:$1_id\tLB:$1_lb\tSM:$1_sm\tPL:illumina") $REF $1_R1.fltr.fastq $1_R2.fltr.fastq | samblaster > $1.sam
 
-	if [ $? -ne 0 ]
-	then				
-		printf "problem at bwa alignment step\n"
-        exit 1
-	else
-		printf "bwa alignment passed at time "
-		date
-	fi	
-
-
-
-###### SAMTOOLS ########################## #######
-###################################################
-
-parallel samtools view -b -S {}.sam ">" {}.temp.bam ::: $(ls -1 *.sam | sed 's/.sam//')	
 	if [ $? -ne 0 ]
 	then				
 		printf "problem at samtools view step\n"
@@ -68,7 +49,25 @@ parallel samtools view -b -S {}.sam ">" {}.temp.bam ::: $(ls -1 *.sam | sed 's/.
 		date
 	fi
 
-parallel samtools sort {}.temp.bam -o {}.sort.bam ::: $(ls -1 *.temp.bam | sed 's/.temp.bam//')
+
+###### SAMTOOLS: SAM TO BAM ################ #######
+###################################################
+
+samtools view -b -S $1.sam > $1.temp.bam 
+	if [ $? -ne 0 ]
+	then				
+		printf "problem at samtools view step\n"
+	        exit 1
+	else
+		printf "samtools view passed at time "
+		date
+	fi
+
+
+###### SAMTOOLS ########################## #######
+###################################################
+
+samtools sort -@ 6 -m 2000M $1.temp.bam -o $1.sort.bam 
 	if [ $? -ne 0 ]
 	then				
 		printf "problem at samtools sort step\n"
@@ -78,7 +77,7 @@ parallel samtools sort {}.temp.bam -o {}.sort.bam ::: $(ls -1 *.temp.bam | sed '
 		date
 	fi
 
-parallel samtools index {} ::: $(ls -1 *.sort.bam)
+samtools index $1.sort.bam 
 	if [ $? -ne 0 ]
 	then				
 		printf "problem at samtools index step\n"
@@ -89,85 +88,160 @@ parallel samtools index {} ::: $(ls -1 *.sort.bam)
         fi
 
 
-
-
-######### DELLY ########################## #######
+###### CNVNATOR ########################## #######
 ###################################################
 
-parallel $BIN/make_dlyconfig.sh {}_delly.cfg ::: $(ls -1 *_1.2-B.sort.bam | sed 's/_1.2-B.sort.bam//')
-
-parallel delly call -o {}_delly.bcf -n -g $REF {}_1.2-B.sort.bam {}_2.2-F.sort.bam ::: $(ls -1 *_1.2-B.sort.bam | sed 's/_1.2-B.sort.bam//')
-
-	printf "[TIMESTAMP]: DELLY  VCF File Created at "
+cnvnator -root $1_cnvn.root -tree $1.temp.bam -chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chr23 chr24 chr25 chr26 chr27 chr28 chr29 chr30 chr31 chr32 chr33 chr34 chr35 chr36 chr37 chr38
+	printf "[TIMESTAMP]: CNVNator -root and -tree completed "
 	date
 
-parallel delly filter -f somatic -o {}_delly_somatic.bcf -s {}_delly.cfg {}_delly.bcf ::: $(ls -1 *_1.2-B.sort.bam | sed 's/_1.2-B.sort.bam//')
+cnvnator -root $1_cnvn.root -his 50 -chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chr23 chr24 chr25 chr26 chr27 chr28 chr29 chr30 chr31 chr32 chr33 chr34 chr35 chr36 chr37 chr38
+	printf "[TIMESTAMP]: CNVNator -root and -his completed "
+	date
 
-parallel delly filter -f germline -o {}_delly_germline.bcf -s {}_delly.cfg {}_delly.bcf ::: $(ls -1 *_1.2-B.sort.bam | sed 's/_1.2-B.sort.bam//')
-
-	printf "[TIMESTAMP]: DELLY  Somatic and Germline Files Created at "
+cnvnator -root $1_cnvn.root -stat 50 $REF -chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chr23 chr24 chr25 chr26 chr27 chr28 chr29 chr30 chr31 chr32 chr33 chr34 chr35 chr36 chr37 chr38
+	printf "[TIMESTAMP]: CNVNator -root and -stat completed "
 	date
 
 
+srun cnvnator -root $1_cnvn.root -partition 50 -chrom chr$SLURM_ARRAY_TASK_ID chr$((SLURM_ARRAY_TASK_ID+1))
+        printf "[TIMESTAMP]: CNVNator -root and -stat completed "
+	date
+
+
+cnvnator -root $1_cnvn.root -call 50 -chrom chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chr23 chr24 chr25 chr26 chr27 chr28 chr29 chr30 chr31 chr32 chr33 chr34 chr35 chr36 chr37 chr38 > $1_cnvn.tsv
+
+	printf "[TIMESTAMP]: CNVNator -root and -call (TSV) completed "
+	date
+		
+	printf "[TIMESTAMP]: CNVNator Somatic and Germline Calls Files Created "
+	date
+	
+cnvnator2VCF.pl cnvn_somatic.tsv $REF > cs.temp.vcf 
+	printf "[TIMESTAMP]: CNVNator Somatic VCF file completed "
+	date
+
+cat $1_cnvn.tsv >> cnvn_all.tsv
+cat $2_cnvn.tsv >> cnvn_all.tsv
+cnvnator2VCF.pl cnvn_all.tsv $REF > ca.temp.vcf 
+	printf "[TIMESTAMP]: CNVNator Combined VCF file completed "
+	date
+
+
+###### VCFTOOLS ########################## #######
+###################################################
+
+vcf-sort -c cs.temp.vcf > cnvn_somatic.vcf
+vcf-sort -c ca.temp.vcf > cnvn_all.vcf
+	printf "[TIMESTAMP]: VCF files Sorted at "
+	date
+
+
+
+##### SAMTOOLS SPLIT #########
+##############################
+
+mkdir bam_splits
+
+samtools view -h -b $1.sort.bam chr$SLURM_ARRAY_TASK_ID > ./bam_splits/$1chr$SLURM_ARRAY_TASK_ID.sort.bam
+	if [ $? -ne 0 ]
+	then				
+		printf "problem at split bam step\n"
+	        exit 1
+	else
+		printf "samtools split bam passed at time "
+		date
+	fi
+
+	
+samtools index ./bam_splits/$1chr$SLURM_ARRAY_TASK_ID.sort.bam
+	if [ $? -ne 0 ]
+	then				
+		printf "problem at split bam indexing step\n"
+	        exit 1
+	else
+		printf "samtools split bam indexing passed at time "
+		date
+	fi
 
 
 ###### BREAKDANCER ########################## #######
 ######################################################
 
-parallel $BIN/bam2cfg.pl -g -h {}_1.2-B.sort.bam {}_2.2-F.sort.bam ">" {}_brkdncr.cfg ::: $(ls -1 *_1.2-B.temp.bam | sed 's/_1.2-B.temp.bam//')
+$BIN/bam2cfg.pl -g -h $1.sort.bam $2.sort.bam > brkdncr.cfg
 
 	printf "[TIMESTAMP]: BRKDNCR Config File Created at "
 	date
 
-parallel breakdancer-max -q 24 {}_brkdncr.cfg ">" {}_brkdncr.tsv ::: $(ls -1 *_1.2-B.temp.bam | sed 's/_1.2-B.temp.bam//')
+mkdir histograms
+mv *histogram* ./histograms
 
-	printf "[TIMESTAMP]: BRKDNCR TSV  File Created at "
-	date
+srun breakdancer-max -o chr$SLURM_ARRAY_TASK_ID -q 24 brkdncr.cfg > chr$SLURM_ARRAY_TASK_ID.tsv 
 
-parallel $BIN/brkdncrCallSomatic.sh {}_brkdncr.tsv {}_brkdncr_somatic.tsv {}_brkdncr_germline.tsv ::: $(ls -1 *_1.2-B.temp.bam | sed 's/_1.2-B.temp.bam//')
 
+for i in {38..1}
+do
+	if [ $i = 1 ]; then
+		mv chr$i.tsv brkdncr_all.tsv
+                break
+        else
+                echo "$(tail -n +7 chr$i.tsv)" > chr$i.tsv
+                cat chr$i.tsv >> chr$((i-1)).tsv
+                rm chr$i.tsv
+        fi
+	done
+
+
+$BIN/brkdncrCallSomatic.sh brkdncr_all.tsv brkdncr_somatic.tsv
 	printf "[TIMESTAMP]: BRKDNCR Somatic Call File Created at "
 	date
 
+$BIN/breakdancer2vcf_JB.py -i brkdncr_all.tsv -o ba.temp.vcf
+vcf-sort -c ba.temp.vcf > brkdncr_all.vcf
+	printf "[TIMESTAMP]: BRKDNCR SOMATIC VCF file Created at "
+	date
 
-parallel $BIN/breakdancer2vcf_JB.py -i {}_brkdncr_somatic.tsv -o {}_brkdncr_somatic.vcf ::: $(ls -1 *_1.2-B.temp.bam | sed 's/_1.2-B.temp.bam//')
-parallel $BIN/breakdancer2vcf_JB.py -i {}_brkdncr_germline.tsv -o {}_brkdncr_germline.vcf ::: $(ls -1 *_1.2-B.temp.bam | sed 's/_1.2-B.temp.bam//')
-
-	printf "[TIMESTAMP]: BRKDNCR VCF files Created at "
+$BIN/breakdancer2vcf_JB.py -i brkdncr_somatic.tsv -o bs.temp.vcf 
+vcf-sort -c bs.temp.vcf > brkdncr_somatic.vcf
+	printf "[TIMESTAMP]: BRKDNCR SOMATIC VCF file Created at "
 	date
 
 
 
-
-
-###### CNVNATOR ########################## #######
+######### DELLY ########################## #######
 ###################################################
 
-parallel cnvnator -root {}_cnvn.root -tree {}.temp.bam -chrom chr1 chr2 chr3 chr4 chr5 chr6 ::: $(ls -1 *.temp.bam | sed 's/.temp.bam//')
-	printf "[TIMESTAMP]: CNVNator -root and -tree completed "
+$BIN/make_dlyconfig3.sh delly.cfg 
+	printf "[TIMESTAMP]: DELLY Config Files Created at "
 	date
 
-parallel cnvnator -root {}_cnvn.root -his 50 -chrom chr1 chr2 chr3 chr4 chr5 chr6 ::: $(ls -1 *.temp.bam | sed 's/.temp.bam//')
-	printf "[TIMESTAMP]: CNVNator -root and -his completed "
+delly call -o chr$SLURM_ARRAY_TASK_ID-delly.bcf -n -g $REF $1chr$SLURM_ARRAY_TASK_ID.sort.bam $2chr$SLURM_ARRAY_TASK_ID.sort.bam 
+	printf "[TIMESTAMP]: DELLY BCF File Created at "
 	date
 
-parallel cnvnator -root {}_cnvn.root -stat 50 $REF -chrom chr1 chr2 chr3 chr4 chr5 chr6 ::: $(ls -1 *.temp.bam | sed 's/.temp.bam//')
-	printf "[TIMESTAMP]: CNVNator -root and -stat completed "
+delly filter -f somatic -o chr$SLURM_ARRAY_TASK_ID-delly_somatic.bcf -s delly.cfg chr$SLURM_ARRAY_TASK_ID-delly.bcf 
+	printf "[TIMESTAMP]: DELLY Somatic BCF Created at "
 	date
 
-parallel cnvnator -root {}_cnvn.root -partition 50 -chrom chr1 chr2 chr3 ::: $(ls -1 *.temp.bam | sed 's/.temp.bam//')
-	printf "[TIMESTAMP]: CNVNator -root and -stat completed "
+
+cd $DATA/bam_splits
+
+
+
+##### BCF CONCAT ###########
+###########################
+ls -1 *-delly.bcf > dellyAll_files
+bcftools concat -o $DATA/a.temp.bcf -f dellyAll_files
+vcf-sort -c $DATA/a.temp.bcf > $DATA/delly_all.bcf
+bcftools view $DATA/delly_all.bcf > $DATA/delly_all.vcf
+	echo "VCFs Merged for ALL Delly Calls at "
 	date
 
-parallel cnvnator -root {}_cnvn.root -call 50 -chrom chr1 chr2 chr3 ">" {}_cnvn.tsv ::: $(ls -1 *.temp.bam | sed 's/.temp.bam//')
-	printf "[TIMESTAMP]: CNVNator -root and -call (TSV) completed "
+ls -1 *-delly_somatic.bcf > dellySomatic_files
+bcftools concat -o $DATA/s.temp.bcf -f dellySomatic_files
+vcf-sort -c $DATA/s.temp.bcf > $DATA/delly_somatic.bcf
+bcftools view $DATA/delly_somatic.bcf > $DATA/delly_somatic.vcf
+	echo "VCFs Merged for SOMATIC Delly Calls at "
 	date
-		
-parallel python $BIN/cnvnCallSomatic.py -normal {}_1.2-B_cnvn.tsv -tumor {}_2.2-F_cnvn.tsv -somatic_out {}_cnvn_somatic.tsv -germline_out {}_cnvn_germline.tsv ::: $(ls -1 *_1.2-B.temp.bam | sed 's/_1.2-B.temp.bam//')
-	printf "[TIMESTAMP]: CNVNator Somatic and Germline Calls Files Created  "
-	date
-	
-parallel cnvnator2VCF.pl {}_cnvn_somatic.tsv $REF ">" {}_cnvn_somatic.vcf ::: $(ls -1 *_1.2-B.temp.bam | sed 's/_1.2-B.temp.bam//')
-parallel cnvnator2VCF.pl {}_cnvn_germline.tsv $REF ">" {}_cnvn_germline.vcf ::: $(ls -1 *_1.2-B.temp.bam | sed 's/_1.2-B.temp.bam//')
-	printf "[TIMESTAMP]: CNVNator VCF files completed "
-	date
+
+
